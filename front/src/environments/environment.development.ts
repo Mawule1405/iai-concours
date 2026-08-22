35354973d814aceb152f20fed9bf1a1293effdb5
HELOU Komlan Mawule <helkmawule@gmail.com>
2026-08-22 19:33:11 +0100

    Modification du paramètre d'environnement

--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8085/iai-concours-api'
+  //apiUrl: 'http://localhost:8085/iai-concours-api'
+  apiUrl: 'https://back-iai-concours.onrender.com/iai-concours-api'
 };
